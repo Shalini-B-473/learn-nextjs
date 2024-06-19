a2e914b66a5e38d6aea267b47253c6ef58a5e2b3
shalini.b <shalini.b@lightcast.io>
2024-06-19 18:23:04 +0530

    Added handling errors code (error.tsx and not-found.tsx)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,11 +23,16 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
+  try{
   await sql`
     INSERT INTO INVOICES (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  }catch(error){
+    return {
+      message: 'Database Error: Failed to create Invoice'
+    }
+  }
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
@@ -39,19 +44,34 @@ export async function updateInvoice(id:string, formData: FormData){
     status : formData.get('status'),
   })
   const amountInCents = amount * 100;
-
+  try{
   await sql`
     UPDATE invoices 
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}
   `;
+  }
+  catch(error){
+    return {
+      message : 'Database Error : Failed to Delete Invoice'
+    }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string){
+  throw new Error ('Failed to Delete Invoice') //comment it out
+  try{
   await sql`
     DELETE FROM invoices WHERE id=${id}
   `;
   revalidatePath('/dashboard/invoices');
+  return { message : 'Deleted invoice' }
+  }
+  catch(error){
+    return {
+      message : 'Database Error : Failed to delete'
+    }
+  }
 }
